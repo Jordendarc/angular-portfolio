199babb6d5cebdf85dca5ef48112d2769ca77c87
Jorden <jorwhitdenbey@gmail.com>
2022-06-11 16:41:06 -0500

    angular 10 -> 11

--- src/app/components/header/header/header.component.spec.ts
+++ src/app/components/header/header/header.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -14,7 +14,7 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ], imports: [ BrowserAnimationsModule, BrowserModule ],
     })
